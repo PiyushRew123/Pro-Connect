e52a04eca518aa54ac3dc4e50931abce19ca2aaf
Piyush_Rew <piyushrewalia12@gmail.com>
2026-01-05 01:41:57 +0530

    Version 1: Ready for Deployment

---
+++ backend/controllers/posts.controllers.js
@@ -0,0 +1,152 @@
+import User from '../models/user.model.js';
+import Profile from '../models/profile.model.js';
+import bcrypt from 'bcrypt';
+import Post from '../models/posts.models.js';
+import Comment from '../models/comments.model.js';
+
+
+export const activeCheck=async(req,res)=>{
+
+    return res.status(200).json({message:"RUNNING"});
+
+}
+
+export const createPost = async (req, res) => {
+    try {
+        const { token, body } = req.body;
+
+        const user = await User.findOne({ token: token });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+
+        const post = new Post({
+            userId: user._id,
+            body: body,
+            media: req.file ? req.file.filename : '',
+            fileTypes: req.file ? req.file.mimetype.split('/') : [],
+        });
+
+        await post.save();
+
+        return res.status(200).json({ message: "Post created successfully" });
+    } catch (error) {
+
+        console.error("Backend Error:", error); 
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const getAllPosts=async(req,res)=>{
+    try{
+        const posts =await Post.find().populate('userId','name username email profilePicture active createdAt');
+        return res.json({posts});
+    }
+    catch(error){
+        
+        return res.status(500).json({message:error.message});
+    }
+}
+export const deletePost=async(req,res)=>{
+    const {token,post_id}=req.body;
+    try{
+
+        const user=await User.findOne({token:token}).select('_id');
+
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+
+        const post= await Post.findOne({_id:post_id});
+
+        if(!post){
+            return res.status(404).json({message:"Post not found"});
+        }
+
+        if(post.userId.toString()!==user._id.toString()){
+            return res.status(401).json({message:"Unauthorized to delete this post"});
+        }   
+
+        await Post.deleteOne({_id:post_id});
+
+        return res.json({message:"Post deleted successfully"});
+    }
+    catch(error){
+        return res.status(500).json({message:error.message});
+    }
+}
+
+export const get_comments_by_post=async(req,res)=>{
+    const {postId}=req.query;    
+   
+    try{
+
+        const post= await Post.findOne({_id:postId});
+        if(!post){
+            return res.status(404).json({message:"Post not found"});
+        }
+
+        const comments= await Comment
+        .find({postId:postId})
+        .populate("userId","username name");
+
+        return res.json(comments.reverse());
+    }
+    catch(error){
+        return res.status(500).json({message:error.message});
+    }   
+}
+
+export  const delete_comment_of_user=async(req,res)=>{
+    const {token,commentId}=req.body;
+    try{
+        const user=await User.findOne({token:token}).select('_id');
+
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+
+        const comment=await Comment.findOne({"_id":commentId});
+
+        if(!comment){
+            return res.status(404).json({message:"Comment not found"});
+        }
+
+        if(comment.userid.toString()!==user._id.toString()){
+            return res.status(401).json({message:"Unauthorized to delete this comment"});
+        }
+
+        await Comment.deleteOne({"_id":commentId});
+
+        return res.json({message:"Comment deleted successfully"});
+
+    }
+    catch(error){
+        return res.status(500).json({message:error.message});
+    }
+}
+
+export const increement_likes=async(req,res)=>{
+
+    const {post_id}=req.body;
+
+    try{
+
+        const post= await Post.findOne({_id:post_id});
+
+        if(!post){
+            return res.status(404).json({message:"Post not found"});
+        }
+
+        post.likes=post.likes+1;
+        await post.save();
+
+        return res.json({message:"Likes increemented successfully",likes:post.likes});
+    }
+    catch(error){
+        return res.status(500).json({message:error.message});
+    }
+
+}
+
